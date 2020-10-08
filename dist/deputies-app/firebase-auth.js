3ec44aa02884ef0ca9c066387f81fff5b80a3dec
Anna Tsvitkova <anna.tsvitkova@outright.digital>
2020-10-08 19:26:55 +0300

    Fix moduls

--- dist/deputies-app/firebase-auth.js
+++ dist/deputies-app/firebase-auth.js
@@ -9,7 +9,7 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _firebase_auth__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @firebase/auth */ "./node_modules/@firebase/auth/dist/auth.js");
+/* harmony import */ var _firebase_auth__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @firebase/auth */ "../../../node_modules/@firebase/auth/dist/auth.js");
 /* harmony import */ var _firebase_auth__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_firebase_auth__WEBPACK_IMPORTED_MODULE_0__);
 
 //# sourceMappingURL=index.esm.js.map
